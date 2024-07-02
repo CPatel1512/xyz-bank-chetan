d2f989a4f611328d6b53f751961be68c4e15dc74
chetan151281 <chetanpatel151281@gmail.com>
2024-07-02 11:20:54 +0100

    HW XYZ Bank

---
+++ src/main/java/com/bank/pages/CustomersPage.java
@@ -0,0 +1,11 @@
+package com.bank.pages;
+
+import com.bank.utilities.Utility;
+import org.apache.log4j.Logger;
+
+public class CustomersPage extends Utility {
+
+    private static final Logger log = Logger.getLogger(CustomersPage.class.getName());
+
+
+}
---
+++ src/main/java/com/bank/pages/HomePage.java
@@ -0,0 +1,9 @@
+package com.bank.pages;
+
+import com.bank.utilities.Utility;
+import org.apache.log4j.Logger;
+
+public class HomePage extends Utility {
+
+    private static final Logger log = Logger.getLogger(HomePage.class.getName());
+}
